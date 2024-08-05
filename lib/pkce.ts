250af3f49965c4a8ea89259fa406e35e15b24641
Masumi Fuyuki <169530032+GD-Fuyuki@users.noreply.github.com>
2024-08-05 14:53:52 +0900

    fix pkce

--- lib/pkce.ts
+++ lib/pkce.ts
@@ -6,10 +6,13 @@ export function generateCodeVerifier(): string {
   return Array.from(array, dec => ('0' + dec.toString(16)).substr(-2)).join('')
 }
 
-export async function generateCodeChallenge(codeVerifier: string): Promise<string> {
-  const digest = sha256(codeVerifier)
-  const digestArray = new Uint8Array(digest)
-  return btoa(String.fromCharCode.apply(null, Array.from(digestArray)))
+export function generateCodeChallenge(codeVerifier: string): string {
+  const hash = sha256(codeVerifier)
+  return base64UrlEncode(hash)
+}
+
+function base64UrlEncode(str: string): string {
+  return btoa(str)
     .replace(/\+/g, '-')
     .replace(/\//g, '_')
     .replace(/=+$/, '')
