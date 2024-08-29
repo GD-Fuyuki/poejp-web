178492eacfeabde48189a80256cb08dd8e1405be
Masumi Fuyuki <169530032+GD-Fuyuki@users.noreply.github.com>
2024-08-29 22:24:03 +0900

    del try

--- lib/poeoauth.ts
+++ lib/poeoauth.ts
@@ -48,23 +48,26 @@ export async function getAccessToken(
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  await sleep(5000);
+  await sleep(1000);
 
-  try {
-    const response = await axios.post(tokenEndpoint, params, config).then(function (response) {
-        console.log(response.data);
-      }).catch(function (error) {
-        console.log(error);
-      });;
+  // try {
+  //   const response = await axios.post(tokenEndpoint, params, config).then(function (response) {
+  //       console.log(response.data);
+  //     }).catch(function (error) {
+  //       console.log(error);
+  //     });;
 
-    return response;
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.response) {
-      console.error('OAuth error:', error.response.status, error.response.data);
-      throw new Error(`OAuth token request failed: ${error.response.statusText}`);
-    } else {
-      console.error('Unexpected error:', error);
-      throw new Error('An unexpected error occurred during the OAuth token request');
-    }
-  }
+  //   return response;
+  // } catch (error) {
+  //   if (axios.isAxiosError(error) && error.response) {
+  //     console.error('OAuth error:', error.response.status, error.response.data);
+  //     throw new Error(`OAuth token request failed: ${error.response.statusText}`);
+  //   } else {
+  //     console.error('Unexpected error:', error);
+  //     throw new Error('An unexpected error occurred during the OAuth token request');
+  //   }
+  // }
+
+  const response = await axios.post(tokenEndpoint, params, config)
+  return response;
 }
