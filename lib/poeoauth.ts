6e58ea4da8c3561dc594e10d70dfb5f938f49a60
Masumi Fuyuki <169530032+GD-Fuyuki@users.noreply.github.com>
2024-08-31 17:20:21 +0900

    fixer

--- lib/poeoauth.ts
+++ lib/poeoauth.ts
@@ -13,48 +13,24 @@ export async function getAccessToken(
     throw new Error('OAuth client credentials are not set in environment variables');
   }
 
-  var params = new URLSearchParams();
-  params.append('client_id', clientId);
-  params.append('client_secret', clientSecret);
-  params.append('grant_type', 'authorization_code');
-  params.append('code', code);
-  params.append('redirect_uri', redirectUri);
-  params.append('scope', 'account:profile account:leagues account:stashes account:characters account:league_accounts');
-  params.append('code_verifier', codeVerifier);
-
-  const config = {
+  const response = await fetch(tokenEndpoint, {
+    method: 'POST',
+    mode: 'no-cors',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  }
+    },
+    body: new URLSearchParams({
+      'client_id': clientId,
+      'client_secret': clientSecret,
+      'grant_type': 'authorization_code',
+      'code': code,
+      'redirect_uri': redirectUri,
+      'scope': 'account:profile account:leagues account:stashes account:characters account:league_accounts',
+      'code_verifier': codeVerifier
+    })
+  })
 
-
-  console.log('param:', params)
-  console.log('endpoint', tokenEndpoint)
-
-  // try {
-  //   const response = await axios.post(tokenEndpoint, params, config).then(function (response) {
-  //       console.log(response.data);
-  //     }).catch(function (error) {
-  //       console.log(error);
-  //     });;
-
-  //   return response;
-  // } catch (error) {
-  //   if (axios.isAxiosError(error) && error.response) {
-  //     console.error('OAuth error:', error.response.status, error.response.data);
-  //     throw new Error(`OAuth token request failed: ${error.response.statusText}`);
-  //   } else {
-  //     console.error('Unexpected error:', error);
-  //     throw new Error('An unexpected error occurred during the OAuth token request');
-  //   }
-  // }
-
-  const tempuri = 'https://qneoaoyizh.execute-api.ap-northeast-1.amazonaws.com/develop/token'
-
-  console.log('starting exchange code to token...')
-  const response: any = await axios.post(tokenEndpoint, params)
-  // const response: any = await axios.post(tempuri, params).then(function (response) {console.log(response);})
   console.log(response)
+
   return response;
 }
